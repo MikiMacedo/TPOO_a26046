a382b4cf35e5d92646ed71ac3ac6297b310c156a
MikiMacedo <a26046@alunos.ipca.pt>
2023-12-02 19:08:14 +0000

    Atualização ClassDiagram1.cd

--- obj/Debug/net7.0/TPOO_a26046.AssemblyInfo.cs
+++ obj/Debug/net7.0/TPOO_a26046.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TPOO_a26046")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5aa17a19e7f9e5c7e3592a88e2b66de27fdb608")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TPOO_a26046")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TPOO_a26046")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
